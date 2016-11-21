a47ab53ed869251b377ceb57c6d27ee87ac6b248
wangyafei <wangyafei@startimes.com.cn>
2016-11-21 20:00:33 +0800

    modify configureStore

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.NODE_ENV);
   module.exports = require('./configureStore.prod');
 } else {
   module.exports = require('./configureStore.dev');
