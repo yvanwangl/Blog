02356944952aca82d1fddcd9c0d5497bd3bed54c
wangyafei <wangyafei@startimes.com.cn>
2016-11-25 21:54:43 +0800

    fix editor bugs

--- client/toggle.js
+++ client/toggle.js
@@ -9,6 +9,8 @@ export default function toggle() {
         var huanStyle = document.createElement('link');
         huanStyle.rel = "stylesheet";
         huanStyle.href = '/huanStyle.css';
+        var title = document.getElementsByTagName('title');
+        title[0].innerHTML="Sunny Huan's Blog";
         document.head.appendChild(huanStyle);
     }
 }
