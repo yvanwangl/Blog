2f258de71bddc6b0b8ce59ab2bda04e9b1b03c41
wangyafei <wangyafei@startimes.com.cn>
2016-11-25 13:06:52 +0800

    modify init count

--- server/proxy.js
+++ server/proxy.js
@@ -1,5 +1,12 @@
 var http = require('http'), 
-	httpProxy = require('http-proxy');  
+	httpProxy = require('http-proxy');
+var https = require('https');
+var fs = require('fs');
+var options = {
+    ca: fs.readFileSync('../www_yvanwang_com/www_yvanwang_com.ca-bundle'),
+    key: fs.readFileSync('../www_yvanwang_com/www_yvanwang_com.key'),
+    cert: fs.readFileSync('../www_yvanwang_com/www_yvanwang_com.crt'),
+};
 	
 // 新建一个代理 Proxy Server 对象
 var proxy = httpProxy.createProxyServer({});
@@ -14,7 +21,7 @@ proxy.on('error', function (err, req, res) {
 
 // 另外新建一个 HTTP 80 端口的服务器，也就是常规 Node 创建 HTTP 服务器的方法。  
 // 在每次请求中，调用 proxy.web(req, res config) 方法进行请求分发  
-var server = require('http').createServer(function(req, res) {
+var server = https.createServer(options, function(req, res) {
   // 在这里可以自定义你的路由分发  
   var host = req.headers.host, ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;  
   console.log("client ip:" + ip + ", host:" + host);  
