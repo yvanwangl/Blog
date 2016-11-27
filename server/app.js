02f675920c5113b52c187b08faf502f093c9f03d
wangyafei <wangyafei@startimes.com.cn>
2016-11-27 21:56:39 +0800

    fix editor bugs

--- server/app.js
+++ server/app.js
@@ -56,27 +56,23 @@ app.use(webpackHotMiddleware(compiler));*/
 // ************************************
 // This is the real meat of the example
 // ************************************
-if (app.get('env') === 'development') {
-    (function() {
+(function() {
 
-        // Step 1: Create & configure a webpack compiler
-        var webpack = require('webpack');
-        var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../webpack.config');
-        var compiler = webpack(webpackConfig);
+    // Step 1: Create & configure a webpack compiler
+    var webpack = require('webpack');
+    var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../webpack.config');
+    var compiler = webpack(webpackConfig);
 
-        // Step 2: Attach the dev middleware to the compiler & the server
-        app.use(require("webpack-dev-middleware")(compiler, {
-            noInfo: true, publicPath: webpackConfig.output.publicPath
-        }));
+    // Step 2: Attach the dev middleware to the compiler & the server
+    app.use(require("webpack-dev-middleware")(compiler, {
+        noInfo: true, publicPath: webpackConfig.output.publicPath
+    }));
 
-        // Step 3: Attach the hot middleware to the compiler & the server
-        app.use(require("webpack-hot-middleware")(compiler, {
-            log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-        }));
-    })();
-}else {
-    console.log('this is production ENV');
-}
+    // Step 3: Attach the hot middleware to the compiler & the server
+    app.use(require("webpack-hot-middleware")(compiler, {
+        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+    }));
+})();
 console.log(app.get('env'));
 
 //set routers
